f3c0fd16ee5fa372bd5dfbe71b10e857f2f75412
Carlos Yepes <carlosyepes021@gmail.com>
2025-08-02 08:52:34 -0500

    Commit en rama2

--- src/main/java/org/example/Promedio.java
+++ src/main/java/org/example/Promedio.java
@@ -1,8 +1,16 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Promedio {
     // metodo para leer un vector de enteros, de tamaño N
     public int []leerVector(int n){
-
+        Scanner scanner = new Scanner(System.in);
+        int[] numero = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Ingrese el numero " + (i+1) + " ");
+            numero[i] =scanner.nextInt();
+        }
+        return numero;
     }
 }
